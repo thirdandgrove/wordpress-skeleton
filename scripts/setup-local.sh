be85e693efee04ec0483b0b062c082c3d9baae2d
Mike DeWolf <michaeldewolf85@gmail.com>
2015-10-14 12:34:33 -0400

    Add basic template for a setup local script.

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-if [[ ! -f prod-data.sh ]] ; then
-    echo 'File "prod-data." has not been created, aborting.'
-    exit
+
+if [[ -f prod-data.sh ]] ; then
+  ./prod-data.sh
 fi
 
+if [[ ! -f ~/dump.sql ]] ; then
+  echo 'No SQL dump found, aborting. Please set one up in prod-data.sh or place one mdump.sql in your home directory'
+  exit
+fi
+
+HOST=$(grep -E "ip: ([^[:space:]]+)" config.yaml | tr -d 'ip: ')
+mysql -uvagrant -pvagrant -h${HOST} -e "drop database if exists vagrant; create database vagrant;"
+mysql -uvagrant -pvagrant -h${HOST} vagrant < ~/dump.sql
+
+rm ~/dump.sql

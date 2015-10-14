19b96172fe7e37bc2bc64c01fe8162db61f0b5d7
anthony <anthony@thirdandgrove.com>
2015-10-14 11:23:03 -0400

    Remove unnecessary DB related definitions. Add instructions for the local config example. Add WP_DEBUG definition. Add local config to gitignore.

--- wp-goodies/wp-config-local-example.php
+++ wp-goodies/wp-config-local-example.php
@@ -3,9 +3,13 @@
  * Local configuration file to add local database to dev environment.
  * To use this file, make a copy and place it in Wordpress root.
  *
- * if (file_exists(dirname(__FILE__) . '/wordpress-skeleton/wp-goodies/wp-config-local.php') {
- *  require_once(dirname(__FILE__) . '/wordpress-skeleton/wp-goodies/wp-config-local.php');
- * }
+ * Place this as the first PHP line in the file.
+ *
+
+  if ( dirname(__FILE__) . '/wordpress-skeleton/wp-goodies/wp-config-local.php' )
+    require_once( dirname(__FILE__) . '/wordpress-skeleton/wp-goodies/wp-config-local.php' );
+
+ *
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
@@ -21,8 +25,4 @@ define('DB_PASSWORD', 'vagrant');
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('WP_DEBUG', true);

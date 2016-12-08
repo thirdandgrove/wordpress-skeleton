1cdde2f59a4e280534bbe1568ba25d536b7a7df0
Matt Brooke <matt@thirdandgrove.com>
2016-12-08 18:39:47 -0500

    Create script to automatically set up new WP environment.

--- scripts/setup-local-new.sh
+++ scripts/setup-local-new.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+. wordpress-skeleton/scripts/vendor/parse_yaml.sh
 
-vagrant ssh
-cd /vagrant
+eval $(parse_yaml wordpress-skeleton/config.yaml "config_")
+
+wp core download
+wp core config --dbname=vagrant --dbuser=vagrant --dbpass=vagrant --ssh=vagrant@$config_hostname/vagrant
+
+echo "Enter site title: "
+read title
+
+echo "Enter admin user: "
+read user
+
+echo "Enter admin password (SAVE THIS TO 1PASSWORD): "
+read password
+
+wp core install --url=$config_hostname --title=$title --admin_user=$user --admin_password=$password --admin_email=support@thirdandgrove.com --skip-email --ssh=vagrant@$config_hostname/vagrant
+
+echo "Enter custom theme name: "
+read theme
+
+svn export https://github.com/thirdandgrove/tag-gulp-starter/trunk wp-content/themes/$theme
+
+rm wp-content/themes/$theme/.gitignore

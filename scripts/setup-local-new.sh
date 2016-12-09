ac4d2abef241fc37a58e9eaf9c0117a857680fca
Matt Brooke <matt@thirdandgrove.com>
2016-12-09 12:07:02 -0500

    Create theme slug, style.css, index.php, and npm install.

--- scripts/setup-local-new.sh
+++ scripts/setup-local-new.sh
@@ -14,13 +14,28 @@ echo "Enter admin user: "
 read user
 
 echo "Enter admin password (SAVE THIS TO 1PASSWORD): "
-read password
+read -s password
 
 wp core install --url=$config_hostname --title=$title --admin_user=$user --admin_password=$password --admin_email=support@thirdandgrove.com --skip-email --ssh=vagrant@$config_hostname/vagrant
 
-echo "Enter custom theme name: "
-read theme
+echo "Enter theme slug: "
+read slug
 
-svn export https://github.com/thirdandgrove/tag-gulp-starter/trunk wp-content/themes/$theme
+echo "Enter theme name: "
+read name
 
-rm wp-content/themes/$theme/.gitignore
+svn export https://github.com/thirdandgrove/tag-gulp-starter/trunk wp-content/themes/$slug
+
+rm wp-content/themes/$slug/.gitignore
+
+cat > wp-content/themes/$slug/style.css << EOF
+/*
+Theme Name: $name
+Author: Third & Grove
+Version 1.0
+*/
+EOF
+
+touch wp-content/themes/$slug/index.php
+
+cd wp-content/themes/$slug && npm install && gulp
